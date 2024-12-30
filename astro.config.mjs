263e0086cf4171b0a4713329a08470d47fb39a1e
Ezequeil Murga OPereyra <exekyel.murga.pereyra@gmail.com>
2024-12-30 11:45:37 -0300

    ready to deploy on vercel.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+
 
 export default defineConfig({
   // ...
+  output: 'server',
   integrations: [tailwind()],
+  adapter: vercel(),
 });
